739de418ec21aef3577de0b44b42f1102c3585ab
Brian <Cycloth76@gmail.com>
2023-08-11 13:31:19 -0700

    Implementing classes, most on the form. a lot of layout work to do still

--- project_420/models.py
+++ project_420/models.py
@@ -9,18 +9,19 @@ class Form(models.Model):
     strain_name = models.CharField(max_length=80)
     strain_base = models.CharField(max_length=80)
     strain_id = models.CharField(max_length=80)
+    grow_info = models.CharField(max_length=500)
     flower_color = models.CharField(max_length=80)
     flower_color2 = models.CharField(max_length=80)
     flower_texture = models.CharField(max_length=80)
     flower_density = models.CharField(max_length=80)
     flower_flavor = models.CharField(max_length=80)
     flower_flavor2 = models.CharField(max_length=80)
-    flower_flavor3 = models.CharField(max_length=80)
+    flower_aroma = models.CharField(max_length=80)
     flower_effect = models.CharField(max_length=80)
     flower_effect2 = models.CharField(max_length=80)
     flower_effect3 = models.CharField(max_length=80)
     user_rating = models.CharField(max_length=80)
-    user_notes = models.CharField(max_length=80)
+    user_notes = models.CharField(max_length=500)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
