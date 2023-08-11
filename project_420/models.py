49d7460108365a007a0ff102050f93ab47df713b
Brian <Cycloth76@gmail.com>
2023-08-11 09:30:24 -0700

    Initial commit after project setup. most classes set up. implementing them on index.html.

---
+++ project_420/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+
+class Form(models.Model):
+    first_name = models.CharField(max_length=80)
+    last_name = models.CharField(max_length=80)
+    email = models.EmailField()
+    date = models.DateField()
+    strain_name = models.CharField(max_length=80)
+    strain_base = models.CharField(max_length=80)
+    strain_id = models.CharField(max_length=80)
+    flower_color = models.CharField(max_length=80)
+    flower_color2 = models.CharField(max_length=80)
+    flower_texture = models.CharField(max_length=80)
+    flower_density = models.CharField(max_length=80)
+    flower_flavor = models.CharField(max_length=80)
+    flower_flavor2 = models.CharField(max_length=80)
+    flower_flavor3 = models.CharField(max_length=80)
+    flower_effect = models.CharField(max_length=80)
+    flower_effect2 = models.CharField(max_length=80)
+    flower_effect3 = models.CharField(max_length=80)
+    user_rating = models.CharField(max_length=80)
+    user_notes = models.CharField(max_length=80)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
---
+++ project_420/views.py
@@ -0,0 +1,51 @@
+from django.shortcuts import render
+from .forms import ApplicationForm
+from .models import Form
+from django.contrib import messages
+from django.core.mail import EmailMessage
+
+
+def index(request):
+    if request.method == "POST":
+        form = ApplicationForm(request.POST)
+        if form.is_valid():
+            first_name = form.cleaned_data["first_name"]
+            last_name = form.cleaned_data["last_name"]
+            email = form.cleaned_data["email"]
+            date = form.cleaned_data["date"]
+            strain_name = form.cleaned_data["strain_name"]
+            strain_base = form.cleaned_data["strain_base"]
+            strain_id = form.cleaned_data["strain_id"]
+            flower_color = form.cleaned_data["flower_color"]
+            flower_color2 = form.cleaned_data["flower_color2"]
+            flower_texture = form.cleaned_data["flower_texture"]
+            flower_density = form.cleaned_data["flower_density"]
+            flower_flavor = form.cleaned_data["flower_flavor"]
+            flower_flavor2 = form.cleaned_data["flower_flavor2"]
+            flower_flavor3 = form.cleaned_data["flower_flavor3"]
+            flower_effect = form.cleaned_data["flower_effect"]
+            flower_effect2 = form.cleaned_data["flower_effect2"]
+            flower_effect3 = form.cleaned_data["flower_effect3"]
+            user_rating = form.cleaned_data["user_rating"]
+            user_notes = form.cleaned_data["user_notes"]
+
+            Form.objects.create(first_name=first_name, last_name=last_name,
+                                email=email, date=date, strain_name=strain_name,
+                                strain_base=strain_base, strain_id=strain_id,
+                                flower_color=flower_color, flower_color2=flower_color2,
+                                flower_texture=flower_texture, flower_density=flower_density,
+                                flower_flavor=flower_flavor, flower_flavor2=flower_flavor2,
+                                flower_flavor3=flower_flavor3, flower_effect=flower_effect,
+                                flower_effect2=flower_effect2, flower_effect3=flower_effect3,
+                                user_rating=user_rating, user_notes=user_notes)
+
+            message_body = f"A new flower review was submitted. Thank you, {first_name}!! We appreciate it!"
+            email_message = EmailMessage("Form submission confirmation", message_body, to=[email])
+            email_message.send()
+
+            messages.success(request, "Form Submitted Successfully!")
+    return render(request, "index.html")
+
+
+def about(request):
+    return render(request, "about.html")
